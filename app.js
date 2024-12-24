c0eaa9df97156cb60004ad59fa8593f5d999c3db
Gustavo Reyes <gustavo.reyes@tutanota.com>
2024-12-24 02:12:11 -0300

    updated gitignore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3002;
+const PORT = process.env.PORT || 3000;
 
 // Serve static files (JavaScript, CSS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
